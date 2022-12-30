16e96afd16ca06edeb92290e375db4dd3cf87675
aadil124 <it.aadil124@gmail.com>
2022-12-30 21:27:48 +0530

    preorder traversal for LL

---
+++ preordertraversal_tree.cpp
@@ -0,0 +1,43 @@
+/***********************************************************************************************
+Implementation of pre order tree
+************************************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+class node
+{
+public:
+    node *left;
+    int data;
+    node *right;
+};
+
+void preorder(node *root)
+{
+
+    cout << root->data << "\t";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main()
+{
+    node *root = NULL;
+    node *b = new node;
+    b->data = 9;
+    b->left = NULL;
+    b->right = NULL;
+    node *c = new node;
+    c->data = 6;
+    c->left = NULL;
+    c->right = NULL;
+    node *a = new node;
+    a->data = 7;
+    a->left = b;
+    a->right = c;
+    root = a;
+    preorder(root);
+
+    return 0;
+}
