8d28952cce8c2ea3c417c83edde1001517aa0b8d
aadil124 <ansariaadil124@gmail.com>
2023-02-18 11:25:25 +0530

    palindromic pattern

--- Mypractice/patterns/palindromicPattern.cpp
+++ Mypractice/patterns/palindromicPattern.cpp
@@ -5,24 +5,18 @@ void palindromicPattern(int n)
 {
   for (int i = 1; i <= n; i++)
   {
+    int k;
     for (int j = 1; j <= n - i; j++)
     {
       cout << " ";
     }
-    for (int k = i; k >= 1; k--)
+    for (k = i; k >= 1; k--)
     {
       cout << k;
     }
-    for (int j = 1; j <= i + 1; j++)
+    for (int s = 2; s <= i; s++)
     {
-      if (j == 1)
-      {
-        cout << " ";
-      }
-      else
-      {
-        cout << j;
-      }
+      cout << s;
     }
 
     cout << endl;
