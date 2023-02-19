900c0eae5797450918bd17a3894d905d765f34c8
aadil124 <ansariaadil124@gmail.com>
2023-02-19 21:32:29 +0530

    diamond star pattern

---
+++ Mypractice/patterns/diamondStarPattern.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void diamondStarPattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter rows for print diamond star pattern: ";
+    cin >> n;
+    diamondStarPattern(n);
+    return 0;
+}
